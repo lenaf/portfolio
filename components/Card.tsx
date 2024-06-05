a845de69fef05f27cc280c718bd0faf1a1ef24c5
lenaf <lforti41@gmail.com>
2024-06-05 19:26:11 -0400

    mobile images

--- components/Card.tsx
+++ components/Card.tsx
@@ -6,60 +6,33 @@ const Card = ({ title, description, desktopImgSrc, mobileImgSrc, href }) => (
     <div
       className={`${
         desktopImgSrc && 'h-full'
-      }  overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
+      } relative overflow-hidden rounded-md border-2 border-gray-200 border-opacity-60 dark:border-gray-700`}
     >
-      <div className="flex">
-        {mobileImgSrc &&
-          (href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              <Image
-                alt={title}
-                src={mobileImgSrc}
-                className="object-cover object-center md:h-36 lg:h-48"
-                width={544}
-                height={306}
-              />
-            </Link>
-          ) : (
-            <Image
-              alt={title}
-              src={mobileImgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          ))}
-        {desktopImgSrc &&
-          (href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              <Image
-                alt={title}
-                src={desktopImgSrc}
-                className="object-cover object-center md:h-36 lg:h-48"
-                width={544}
-                height={306}
-              />
-            </Link>
-          ) : (
-            <Image
-              alt={title}
-              src={desktopImgSrc}
-              className="object-cover object-center md:h-36 lg:h-48"
-              width={544}
-              height={306}
-            />
-          ))}
-      </div>
-
+      {mobileImgSrc && (
+        <Link href={href} aria-label={`Link to ${title}`}>
+          <Image
+            alt={title}
+            src={mobileImgSrc}
+            className="contain-center absolute left-48 top-10 object-cover md:h-48 lg:h-64"
+            width={200}
+            height={306}
+          />
+        </Link>
+      )}
+      <Link href={href} aria-label={`Link to ${title}`}>
+        <Image
+          alt={title}
+          src={desktopImgSrc}
+          className="object-contain object-center md:h-48 lg:h-64"
+          width={544}
+          height={306}
+        />
+      </Link>
       <div className="p-6">
         <h2 className="mb-3 text-2xl font-bold leading-8 tracking-tight">
-          {href ? (
-            <Link href={href} aria-label={`Link to ${title}`}>
-              {title}
-            </Link>
-          ) : (
-            title
-          )}
+          <Link href={href} aria-label={`Link to ${title}`}>
+            {title}
+          </Link>
         </h2>
         <p className="prose mb-3 max-w-none text-gray-500 dark:text-gray-400">{description}</p>
         {href && (
--- data/projectsData.ts
+++ data/projectsData.ts
@@ -11,30 +11,46 @@ interface Project {
 
 const projectsData: Project[] = [
   {
+    title: 'Loondog Ceramics E-Commerce Site',
+    description: `UX Design, UI Design, Developer, Ceramic Artist`,
+    stack: `Hydrogen, Shopify`,
+    desktopImgSrc: '/static/images/projects/loondog-desktop.png',
+    mobileImgSrc: '/static/images/projects/loondog-mobile.png',
+    href: 'https://www.loondog.com',
+  },
+  {
     title: 'Mariposa Gallery Exhibit Static Site',
     description: `UX Design, Developer`,
     descriptionSecondLine: `Built and designed.`,
     stack: `Gatsby, Tailwind`,
-    desktopImgSrc: '/static/images/projects/mariposa-deskop.png',
+    desktopImgSrc: '/static/images/projects/mariposa-desktop.png',
     mobileImgSrc: '/static/images/projects/mariposa-mobile.png',
     href: 'https://www.mariposa.gallery',
     github: '',
   },
   {
-    title: 'Loondog Ceramics E-Commerce Site',
-    description: `UX Design, UI Design, Developer, Ceramic Artist`,
-    stack: `Hydrogen, Shopify`,
-    desktopImgSrc: '/static/images/projects/loondog-deskop.png',
-    mobileImgSrc: '/static/images/projects/loondog-mobile.png',
-    href: 'https://www.loondog.com',
-  },
-  {
     title: 'Interpretting Business Marketing Static Site',
     description: `UX Design, UI Design, Developer`,
     stack: `Gatsby, Tailwind`,
-    desktopImgSrc: '/static/images/projects/interpretelle-deskop.png',
+    desktopImgSrc: '/static/images/projects/interpretelle-desktop.png',
     mobileImgSrc: '/static/images/projects/interpretelle-mobile.png',
     href: 'https://www.interpretelle.com',
+  },
+  {
+    title: 'Hulu Web Browsing',
+    description: `Developer`,
+    descriptionSecondLine: `Built and designed.`,
+    stack: `Gatsby, Tailwind`,
+    desktopImgSrc: '/static/images/projects/hulu-desktop.png',
+    href: 'https://www.hulu.com',
+  },
+  {
+    title: 'Kangarootime',
+    description: `UX Design, Developer`,
+    descriptionSecondLine: `Built and designed.`,
+    stack: `Gatsby, Tailwind`,
+    desktopImgSrc: '/static/images/projects/kangarootime-desktop.png',
+    href: 'https://www.hulu.com',
   },
 ]
 
