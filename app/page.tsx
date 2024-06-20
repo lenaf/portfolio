b44d69317c8ed14cdaf23ae4bc934e20724bcaa6
lenaf <lforti41@gmail.com>
2024-06-19 21:21:47 -0400

    text

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,14 @@ export default async function Page() {
     <>
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
-          <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
-            Hi, I'm Lena.
+          <h1 className="text-lg leading-9 tracking-tight text-gray-900 dark:text-gray-100 ">
+            Hello, my name is Lena Forti
           </h1>
-          <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
-            I'm a web developer, designer, ceramacist, and nature/animal person.{' '}
-          </p>
+          <div className="flex">
+            <p className="leading-18 text-6xl text-gray-900 dark:text-gray-400">
+              I'm a web developer, designer, and ceramacist.
+            </p>
+          </div>
         </div>
       </div>
       {siteMetadata.newsletter?.provider && (
