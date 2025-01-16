73f70e47d711c6802797e02341131e88765676fa
lenaf <lforti41@gmail.com>
2025-01-16 09:51:28 -0500

    update

--- app/page.tsx
+++ app/page.tsx
@@ -7,11 +7,14 @@ export default async function Page() {
       <div className="divide-y divide-gray-200 dark:divide-gray-700">
         <div className="space-y-2 pb-8 pt-6 md:space-y-5">
           <h1 className="text-lg leading-9 tracking-tight text-gray-900 dark:text-gray-100 ">
-            Hello, my name is Lena Forti
+            Hello, my name is Lena Forti.
           </h1>
           <div className="flex">
             <p className="leading-18 text-6xl text-gray-900 dark:text-gray-400">
-              I'm a web developer, designer, and ceramacist.
+              I'm a web developer and freelance UI/UX designer.
+              {/* I am a full-stack web developer with an eye for UI/UX design.
+              I make websites that run fast and feel effortless and fun to use.
+              Check out some of my work. */}
             </p>
           </div>
         </div>
