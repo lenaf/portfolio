e7ac89d307d98a15d4eca4bb20b801c4520c6ff2
lena.forti <lena.forti@disneystreaming>
2025-06-23 15:55:56 -0400

    fix build

--- app/page.tsx
+++ app/page.tsx
@@ -40,11 +40,11 @@ export default async function Page() {
           </div>
         </div>
       </div>
-      {siteMetadata.newsletter?.provider && (
+      {/* {siteMetadata.newsletter?.provider && (
         <div className="flex items-center justify-center pt-4">
           <NewsletterForm />
         </div>
-      )}
+      )} */}
     </>
   )
 }
